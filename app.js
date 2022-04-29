f6a67fad51213d545a4e9acc95dc79451a3cd4ff
Javad Moulaei <ijavadmoulaei@gmail.com>
2022-04-30 04:25:45 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+const connectDB = require("./config/db");
+const indexRoutes = require("./routes");
+
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+const app = express();
+
+if (process.env.NODE_ENV == "development") app.use(morgan("dev"));
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(indexRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
