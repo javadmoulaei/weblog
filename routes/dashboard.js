3c7dfeef5857f16e43bfb3a308315b147dd75e5a
Javad Moulaei <ijavadmoulaei@gmail.com>
2022-05-01 01:15:47 +0430

    add dashboard page

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,6 +2,14 @@ const { Router } = require("express");
 
 const router = new Router();
 
+router.get("/", (req, res) => {
+  res.render("dashboard", {
+    pageTitle: "dashboard",
+    path: "/dashboard",
+    layout: "./layouts/dashboard",
+  });
+});
+
 router.get("/login", (req, res) => {
   res.render("login", { pageTitle: "sign-in", path: "/login" });
 });
