81385fb3264105aab660a35158c5344560f11876
Javad Moulaei <ijavadmoulaei@gmail.com>
2022-05-04 17:05:54 +0430

    refactore code and un fastest-validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,55 +1,8 @@
 const { Router } = require("express");
-const Validator = require("fastest-validator");
 
+const User = require("../models/User");
 const router = new Router();
-const validator = new Validator();
 
-const schema = {
-  fullname: {
-    type: "string",
-    trim: true,
-    min: 4,
-    max: 255,
-    optional: false,
-    messages: {
-      required: "نام و نام خانوادگی الزامی می باشد",
-      stringMin: "نام و نام خانوادگی نباید کمتر از 4 کاراکتر باشد",
-      stringMax: "نام و نام خانوادگی نباید بیشتر از 255 کاراکتر باشد",
-    },
-  },
-  email: {
-    type: "email",
-    normalize: true,
-    messages: {
-      emailEmpty: "فیلد ایمیل نباید خالی باشد",
-      required: "ایمیل الزامی می باشد",
-      string: "آدرس ایمیل را بررسی کنید",
-    },
-  },
-  password: {
-    type: "string",
-    min: 4,
-    max: 255,
-    messages: {
-      required: "کلمه عبور الزامی می باشد",
-      string: "کلمه عبور را بررسی کنید",
-      stringMin: "کلمه عبور نباید کمتر از 4 کاراکتر باشد",
-      stringMax: "کلمه عبور نمی تواند بیشتر از 255 کاراکتر باشد",
-    },
-  },
-  confirmPassword: {
-    type: "string",
-    min: 4,
-    max: 255,
-    messages: {
-      required: "تکرار کلمه عبور الزامی می باشد",
-      string: "تکرار کلمه عبور را بررسی کنید",
-      stringMin: "تکرار کلمه عبور نباید کمتر از 4 کاراکتر باشد",
-      stringMax: "تکرار کلمه عبور نباید بیشتر از 255 کاراکتر باشد",
-    },
-  },
-  $$strict: true,
-};
 router.get("/login", (req, res) => {
   res.render("login", { pageTitle: "ورود", path: "/login" });
 });
@@ -62,26 +15,26 @@ router.get("/register", (req, res) => {
 });
 
 router.post("/register", async (req, res) => {
-  const validate = validator.validate(req.body, schema);
-  let errors = [];
-  if (validate === true) {
-    const { password, confirmPassword } = req.body;
-    if (password !== confirmPassword) {
-      errors.push({ message: "رمز عبور و تکرار رمز عبور یکسان نیستند." });
+  try {
+    await User.userValidation(req.body);
+    res.redirect("/auth/login");
+  } catch (error) {
+    console.log(error);
+    const errors = [];
 
-      return res.render("register", {
-        pageTitle: "ثبت نام",
-        path: "/register",
-        errors,
+    error.inner.forEach((element) => {
+      errors.push({
+        name: element.path,
+        message: element.message,
       });
-    }
-    return res.redirect("/auth/login");
+    });
+
+    return res.render("register", {
+      pageTitle: "ثبت نام",
+      path: "/register",
+      errors,
+    });
   }
-  return res.render("register", {
-    pageTitle: "ثبت نام",
-    path: "/register",
-    errors: validate,
-  });
 });
 
 module.exports = router;
