42b57ad451a65b1b591c7cdbc85a668622eac6af
Javad Moulaei <ijavadmoulaei@gmail.com>
2022-05-05 14:10:30 +0430

    feature register

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -8,12 +8,26 @@ exports.get = (req, res) => {
 };
 
 exports.post = async (req, res) => {
+  const errors = [];
   try {
     await User.userValidation(req.body);
+
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      errors.push({ message: "کاربری با این ایمیل ثبت نام کرده است." });
+
+      return res.render("register", {
+        pageTitle: "ثبت نام",
+        path: "/register",
+        errors,
+      });
+    }
+
+    await User.create(req.body);
     res.redirect("/auth/login");
   } catch (error) {
     console.log(error);
-    const errors = [];
 
     error.inner.forEach((element) => {
       errors.push({
