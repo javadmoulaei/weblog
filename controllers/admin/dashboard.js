7bc5aab70b5337bc2ba3521b620f46d1592a2335
Javad Moulaei <ijavadmoulaei@gmail.com>
2022-05-17 16:10:42 +0430

    feature show posts in dashboard

--- controllers/admin/dashboard.js
+++ controllers/admin/dashboard.js
@@ -1,8 +1,17 @@
+const Blog = require("../../models/Blog");
+
 exports.get = async (req, res) => {
-  res.render("private/blogs", {
-    pageTitle: "بخش مدیریت",
-    path: "/dashboard",
-    layout: "./layouts/dashboard",
-    fullname: req.user.fullname,
-  });
+  try {
+    const blogs = await Blog.find({ user: req.user.id });
+
+    res.render("private/blogs", {
+      pageTitle: "بخش مدیریت",
+      path: "/dashboard",
+      layout: "./layouts/dashboard",
+      fullname: req.user.fullname,
+      blogs,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
