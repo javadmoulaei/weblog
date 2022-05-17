f9c076983a500d84fe76a833f47865acebe2a117
Javad Moulaei <ijavadmoulaei@gmail.com>
2022-05-18 02:54:28 +0430

    refactore logout

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    unset: "destroy",
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
   })
 );
--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -2,8 +2,8 @@ const { get500 } = require("../errors");
 
 exports.get = (req, res) => {
   try {
+    req.session = null;
     req.logout();
-    req.flash("success_msg", "با موفقیت خارج شدید.");
     res.redirect("/auth/login");
   } catch (error) {
     get500(req, res, error);
