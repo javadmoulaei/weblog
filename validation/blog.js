e6fd0b26394ee0c7ea27ac5de964a8a9cd97c31c
Javad Moulaei <ijavadmoulaei@gmail.com>
2022-05-23 17:14:57 +0430

    develop validation upload with yup

--- validation/blog.js
+++ validation/blog.js
@@ -8,4 +8,14 @@ exports.schema = yup.object().shape({
     .max(100, "عنوان نباید بیشتر از 100 کاراکتر باشد"),
   body: yup.string().required("محتوا الزامی می باشد"),
   status: yup.mixed().oneOf(["public", "private"], "وضعیت نامعتبر"),
+  thumbnail: yup.object().shape({
+    name: yup.string().required("عکس بند انگشتی الزامی میباشد"),
+    size: yup.string().max(3000000, "عکس نباید بیشتر از ۳ مگابایت باشد"),
+    mimetype: yup
+      .mixed()
+      .oneOf(
+        ["image/jpeg", "image/png"],
+        "تنها پسوندهای png و jpeg پشتیبانی میشوند."
+      ),
+  }),
 });
